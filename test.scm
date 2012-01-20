f8af64739cea1824f13c69f9789144c7b793f544
Andrei Kopats <hlamer@tut.by>
2012-01-20 19:12:31 +0300

    x

--- test.scm
+++ test.scm
@@ -32,3 +32,44 @@
 
 (define (edge2-frame f)
   (car (cdr (cdr f))))
+
+(define (make-segment p1 p2)
+  (cons p1 p2))
+
+(define (start-segment p)
+  (car p))
+
+(define (end-segment p)
+  (cdr p))
+
+(define (draw-line p1 p2)
+  (display "line ")
+  (display p1)
+  (display p2)
+  (newline))
+
+(define (segments->painter segment-list)
+  (lambda (frame)
+    (for-each
+     (lambda (segment)
+       (draw-line
+        ((frame-coord-map frame) (start-segment segment))
+        ((frame-coord-map frame) (end-segment segment))))
+     segment-list)))
+
+(define (paint-frame f)
+  (segments->painter (list (origin-frame f)
+                           (edge1-frame f)
+                           (add-vect (edge1-frame f) (edge2-frame f))
+                           (edge2-frame f)
+                           (origin-frame f))))
+
+(define canvas (make-frame (make-vector 0 0)
+                           (make-vector 0 100)
+                           (make-vector 100 0)))
+
+(define f (make-frame (make-vector 1 1)
+                      (make-vector 0 4)
+                      (make-vector 5 0)))
+
+(paint-frame f)
